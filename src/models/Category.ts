bb72da2aef9afead4f1ae6ce9486f77e26a4c0ca
Jonathan <jonathandias96@gmail.com>
2020-07-12 11:31:08 -0300

    Create models

--- src/models/Category.ts
+++ src/models/Category.ts
@@ -1,10 +1,22 @@
+import {
+   Column,
+   CreateDateColumn,
+   UpdateDateColumn,
+   PrimaryGeneratedColumn,
+  } from 'typeorm'
+
 class Category {
+
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Column()
   title: string;
 
+  @CreateDateDolumn()
   created_at: Date;
 
+  @UpdateDateColumn()
   updated_at: Date;
 }
 
